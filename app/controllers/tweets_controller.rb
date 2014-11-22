e98c7c23b44a3df55fe76c0e1b1445eb4b873fbc
lucyq <lucyzqin@gmail.com>
2014-11-22 16:10:27 -0500

    tried to add user id to tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,10 @@ class TweetsController < ApplicationController
 	end
 
 	def create
+		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
+		@tweet.save
+
 		@tweet = Tweet.create(tweet_params)
 		@tweets = Tweet.all
 		flash[:success] = "You have created a tweet!"
